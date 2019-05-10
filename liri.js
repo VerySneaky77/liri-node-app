97b0774d7a855449047bb76d894b34d8e130dd52
Carlos Quinto <carlquin77@gmail.com>
2019-05-10 14:40:31 -0700

    added OMDB API functionality

--- liri.js
+++ liri.js
@@ -7,23 +7,60 @@ var moment = require("moment");
 
 var spotify = new Spotify(keys.spotify);
 var actionRequest = process.argv[2];
+var request = fixRequest();
 
-switch(actionRequest) {
+switch (actionRequest) {
     case "concert-this":
-    var dataRequest = "";
 
-    for (let i = 3; i < process.argv.length; i++) {
-        dataRequest += process.argv[i];
-    }
-    concertThis(dataRequest);
+        concertThis();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+
+    default:
+        console.log("Invalid command. \nEnter one of the following commands on startup:\nconcert-this\n");
 }
 
-function concertThis(artist) {
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response) {
+// Build full strings from process.argv input
+function fixRequest() {
+    var strBuilder = "";
+
+    for (let i = 3; i < process.argv.length; i++) {
+        strBuilder += process.argv[i];
+    }
+    return strBuilder;
+}
+
+// Bands In Town API information
+function concertThis() {
+    axios.get("https://rest.bandsintown.com/artists/" + request + "/events?app_id=codingbootcamp").then(function (response) {
         var concertStuff = response.data;
 
-        for (let ii = 0; ii < concertStuff; ii++) {
-            console.log(concertStuff[ii].venue);
+        for (let ii = 0; ii < concertStuff.length; ii++) {
+            console.log("\nVenue: " + concertStuff[ii].venue.name);
+            console.log("City, Country: " + concertStuff[ii].venue.city + ", " + concertStuff[ii].venue.country);
+            console.log("Date: " + moment(concertStuff[ii].datetime).format("MM/DD/YYYY"));
         }
+    });
+}
+
+// OMDB API information
+function movieThis() {
+    axios.get("http://www.omdbapi.com/?t=" + request + "&y=&plot=short&apikey=trilogy").then(function (response) {
+        var movieStuff = response.data;
+
+        console.log("Title: " + movieStuff.Title);
+        console.log("Release Year: " + movieStuff.Year);
+        console.log("Country of Production: " + movieStuff.Country);
+        console.log("Language: " + movieStuff.Language);
+        console.log("Plot Synopsis: " + movieStuff.Plot);
+        console.log("IMDB Rating: " + movieStuff.imdbRating);
+        console.log(movieStuff.Ratings[1].Source + " Rating: " + movieStuff.Ratings[1].Value);
     });
 }
