d59004dd1129549b59a14f649368053424537f3f
Carlos Quinto <carlquin77@gmail.com>
2019-05-13 23:00:31 -0700

    spotify functionality added, all working

--- liri.js
+++ liri.js
@@ -4,55 +4,50 @@ var keys = require("./keys.js");
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
 var moment = require("moment");
-
+const fs = require("fs");
 var spotify = new Spotify(keys.spotify);
+
+// Valid commands
+const commandsList = ["concert-this", "movie-this", "spotify-this-song", "do-what-it-says"];
+const commandsInstruct = ["\t<artist/band name>", "\t<title of movie>", "\t<song title and artist name>", ""];
+
 var actionRequest = process.argv[2];
-var request = fixRequest();
-
-switch (actionRequest) {
-    case "concert-this":
-
-        concertThis();
-        break;
-
-    case "movie-this":
-        movieThis();
-        break;
-
-    case "spotify-this-song":
-        spotifyThisSong();
-        break;
-
-    default:
-        console.log("Invalid command. \nEnter one of the following commands on startup:\nconcert-this\n");
-}
 
 // Build full strings from process.argv input
-function fixRequest() {
-    var strBuilder = "";
+function fixRequest(defaultVal) {
+    if (process.argv.length < 4) { return defaultVal; }
+    else {
+        var strBuilder = [];
 
-    for (let i = 3; i < process.argv.length; i++) {
-        strBuilder += process.argv[i];
+        for (let i = 3; i < process.argv.length; i++) {
+            strBuilder.push(process.argv[i]);
+        }
+        console.log(strBuilder);
+        return strBuilder;
     }
-    return strBuilder;
 }
 
 // Bands In Town API information
-function concertThis() {
-    axios.get("https://rest.bandsintown.com/artists/" + request + "/events?app_id=codingbootcamp").then(function (response) {
-        var concertStuff = response.data;
+function concertThis(request) {
+    if (request === false) {
+        console.log("You need to supply an artist name.");
+    }
+    else {
+        axios.get("https://rest.bandsintown.com/artists/" + request.join(" ") + "/events?app_id=codingbootcamp").then(function (response) {
+            var concertStuff = response.data;
 
-        for (let ii = 0; ii < concertStuff.length; ii++) {
-            console.log("\nVenue: " + concertStuff[ii].venue.name);
-            console.log("City, Country: " + concertStuff[ii].venue.city + ", " + concertStuff[ii].venue.country);
-            console.log("Date: " + moment(concertStuff[ii].datetime).format("MM/DD/YYYY"));
-        }
-    });
+            for (let ii = 0; ii < concertStuff.length; ii++) {
+                console.log("\nVenue: " + concertStuff[ii].venue.name);
+                console.log("City, Country: " + concertStuff[ii].venue.city + ", " + concertStuff[ii].venue.country);
+                console.log("Date: " + moment(concertStuff[ii].datetime).format("MM/DD/YYYY"));
+            }
+        });
+    }
 }
 
 // OMDB API information
-function movieThis() {
-    axios.get("http://www.omdbapi.com/?t=" + request + "&y=&plot=short&apikey=trilogy").then(function (response) {
+function movieThis(request) {
+    axios.get("http://www.omdbapi.com/?t=" + request.join(" ") + "&y=&plot=short&apikey=trilogy").then(function (response) {
         var movieStuff = response.data;
 
         console.log("Title: " + movieStuff.Title);
@@ -63,4 +58,77 @@ function movieThis() {
         console.log("IMDB Rating: " + movieStuff.imdbRating);
         console.log(movieStuff.Ratings[1].Source + " Rating: " + movieStuff.Ratings[1].Value);
     });
+}
+
+// Spotify song API information
+function spotifyThis(request) {
+
+    spotify.search({ type: "track", query: request.join(" "), limit: 10 }, function (error, response) {
+        if (error) {
+            return console.log(error);
+        }
+
+        spotifyDisplay(response.tracks.items[0]);
+    });
+}
+
+function spotifyDisplay(data) {
+    var output = [
+        "Artist(s): ",
+        "Song Title: ",
+        "Preview: ",
+        "Album Title: "
+    ]
+
+    var outputTemp = "";
+
+    // Select artists
+    outputTemp = output[0];
+    data.artists.forEach(function (artist) {
+        outputTemp += artist.name;
+    });
+    output[0] = outputTemp;
+
+    // Select song title
+    outputTemp = output[1];
+    outputTemp += data.name;
+    output[1] = outputTemp;
+
+    // Select preview link
+    outputTemp = output[2];
+    outputTemp += data.preview_url;
+    output[2] = outputTemp;
+
+    // Select album title
+    outputTemp = output[3];
+    outputTemp += data.album.name;
+    output[3] = outputTemp;
+
+    output.forEach(function (info) {
+        console.log(info);
+    })
+}
+
+// Read action request input
+switch (actionRequest) {
+    case commandsList[0]:
+        concertThis(fixRequest(false));
+        break;
+
+    case commandsList[1]:
+        movieThis(fixRequest(["Mr. Nobody"]));
+        break;
+
+    case commandsList[2]:
+        spotifyThis(fixRequest(["The Sign", "Ace of Base"]));
+        break;
+
+    default:
+        console.log("Invalid command. Enter one of the following commands on startup:");
+
+        for (let i = 0; i < commandsList.length; i++) {
+            console.log(commandsList[i] + commandsInstruct[i]);
+        }
+
+        break;
 }
